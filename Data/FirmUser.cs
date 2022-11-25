4f02e47c6a4e354a62036406e820bcdee7de05e8
joxx0181 <62244562+joxx0181@users.noreply.github.com>
2022-11-25 08:47:00 +0100

    Updates

--- Data/FirmUser.cs
+++ Data/FirmUser.cs
@@ -2,12 +2,14 @@
 
 namespace TheMall.Data
 {
-    public class UserLogin
+    public class FirmUser
     {
         [Required]
         public string? UserName { get; set; }
 
         [Required]
         public string? Password { get; set; }
+
+        public string? Role { get; set; }
     }
 }
