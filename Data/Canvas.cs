a0dc03e33661c44c1c1b90c641035d5c4ffb1e7e
SweetDS <85709955+Baadlugen@users.noreply.github.com>
2022-12-05 13:57:28 +0100

    CanvasDrawing

--- Data/Canvas.cs
+++ Data/Canvas.cs
@@ -14,29 +14,37 @@ namespace TheMall.Data
         /// <param name="baseStrokeColor"></param>
         /// <param name="width"></param>
         /// <param name="height"></param>
-        public async void DrawMap(Canvas2DContext ctx, List<ComponentV> components,string baseStrokeColor,double width,double height)
+        public async void DrawMap(Canvas2DContext ctx, List<ComponentV> components, string baseStrokeColor, double width, double height)
         {
-
+            await ctx.SetStrokeStyleAsync(baseStrokeColor);
+            await ctx.ClearRectAsync(0, 0, width, height).ConfigureAwait(false);
             bool firstDraw = true;
             await ctx.SetTransformAsync(1, 0, 0, 1, 0, 0);
+            await ctx.BeginBatchAsync();
             for (int j = 0; j < components.Count; j++)
             {
-                await ctx.BeginBatchAsync();
+
                 for (int x = 0; x < components[j].Geodata.XInput.Length; x++)
                 {
                     if (firstDraw == true)
                     {
+                        await ctx.BeginPathAsync();
                         await ctx.MoveToAsync(components[j].Geodata.XInput[x], components[j].Geodata.YInput[x]);
                         firstDraw = false;
                     }
                     else
+                    {
                         await ctx.LineToAsync(components[j].Geodata.XInput[x], components[j].Geodata.YInput[x]);
+                        await ctx.StrokeAsync();
+                    }
                 }
-                await ctx.EndBatchAsync();
+
+                await ctx.ClosePathAsync();
+
                 firstDraw = true;
             }
-            await ctx.StrokeAsync();
-            await ctx.SetStrokeStyleAsync(baseStrokeColor);
+            await ctx.EndBatchAsync();
+           
         }
     }
 
